9374fb869a7dc7015e1c9f4118ca23fb8d9323ae
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-22 03:21:51 +0700

    Update ins.sh

--- apk/ins.sh
+++ apk/ins.sh
@@ -7,12 +7,13 @@ apt install curl -y
 wget -q -O bot "https://raw.githubusercontent.com/rullpqh/installer/main/config/bot.php" && chmod +x bot
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rullpqh/installer/main/config/menu.sh" && chmod +x /usr/bin/menu
 
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
 if [ "$BASH" ]; then
   if [ -f ~/.bashrc ]; then
     . ~/.bashrc
   fi
 fi
-
 mesg n || true
 clear
 menu
@@ -20,3 +21,9 @@ END
 chmod 644 /root/.profile
 clear
 menu
+
+
+
+
+
+
